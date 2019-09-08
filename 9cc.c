c25a4c37894e4a63e808879c1af989ef6ee4cb29
nekonabesan <sheepdogs_2@hotmail.com>
2019-09-08 19:17:06 +0900

    https://github.com/rui314/9cc/commit/916237396164ae25557b62bf0d1ca9d9cf8c2070

--- 9cc.c
+++ 9cc.c
@@ -7,10 +7,30 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  char *p = argv[1];
+
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
-  printf("  mov rax, %ld\n", strtol(argv[1], NULL, 10));
+  printf("  mov rax, %ld\n", strtol(p, &p, 10));
+
+  while (*p) {
+      if (*p == '+') {
+        p++;
+        printf("  add rax, %ld\n", strtol(p, &p, 10));
+        continue;
+      }
+
+      if (*p == '-') {
+        p++;
+        printf("  sub rax, %ld\n", strtol(p, &p, 10));
+        continue;
+      }
+
+      fprintf(stderr, "unexpected charactor: %c\n", *p);
+      return 1;
+  }
+
   printf("  ret\n");
   return 0;
 }
