b596662b6c0a64f55070ce0b48e58c5fab41b32f
nekonabesan <sheepdogs_2@hotmail.com>
2019-09-11 17:38:45 +0900

    split into multiple .c files
    https://github.com/rui314/9cc/commit/ce9a6bbfbd430332666c64cd8b433043c45a6044

---
+++ token.c
@@ -0,0 +1,46 @@
+#include "9cc.h"
+
+// Tokenizer
+static Token *add_token(Vector *v, int ty, char *input) {
+  Token *t = malloc(sizeof(Token));
+  t->ty = ty;
+  t->input = input;
+  vec_push(v, t);
+  return t;
+}
+
+// Tokennized input is stored to this array
+Vector *tokenize(char *p) {
+  Vector *v = new_vec();
+
+  int i = 0;
+  while (*p) {
+    // Skip white space
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
+    // + or -
+    if (*p == '+' || *p == '-') {
+      add_token(v, *p, p);
+      i++;
+      p++;
+      continue;
+    }
+
+    // Number
+    if (isdigit(*p)) {
+      Token *t = add_token(v, TK_NUM, p);
+      t->val = strtol(p, &p, 10);
+      i++;
+      continue;
+    }
+
+    fprintf(stderr, "cannot tokennize: %s\n", p);
+
+  }
+
+  add_token(v, TK_EOF, p);
+  return v;
+}
