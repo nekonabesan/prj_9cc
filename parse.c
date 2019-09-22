b951c9ffd3d1bde4cb68539ba820c9306b0f680a
nekonabesan <sheepdogs_2@hotmail.com>
2019-09-22 15:02:33 +0900

    Simplify.
    https://github.com/rui314/9cc/commit/24e1eecf976eaf12e4cd24691f99491767166c99

--- parse.c
+++ parse.c
@@ -26,11 +26,18 @@ static bool is_typename() {
   return t->ty == TK_INT;
 }
 
-static Node *new_node(int op, Node *lhs, Node *rhs) {
+static Node *new_binop(int op, Node *lhs, Node *rhs) {
   Node *node = calloc(1, sizeof(Node));
   node->op = op;
   node->lhs = lhs;
   node->rhs = rhs;
+  return node;
+}
+
+static Node *new_expr(int op, Node *expr) {
+  Node *node = calloc(1, sizeof(Node));
+  node->op = op;
+  node->expr = expr;
   return node;
 }
 
@@ -80,10 +87,7 @@ static Node *mul();
 static Node *postfix() {
   Node *lhs = primary();
   while (consume('[')) {
-    Node *node = calloc(1, sizeof(Node));
-    node->op = ND_DEREF;
-    node->expr = new_node('+', lhs, primary());
-    lhs = node;
+    lhs = new_expr(ND_DEREF, new_binop('+', lhs, primary()));
     expect(']');
   }
   return lhs;
@@ -91,22 +95,14 @@ static Node *postfix() {
 
 static Node *unary() {
   if (consume('*')) {
-    Node *node = calloc(1, sizeof(Node));
-    node->op = ND_DEREF;
-    node->expr = mul();
-    return node;
+    return new_expr(ND_DEREF, mul());
   }
   if (consume('&')) {
     Node *node = calloc(1, sizeof(Node));
-    node->op = ND_ADDR;
-    node->expr = mul();
-    return node;
+    return new_expr(ND_ADDR, mul());
   }
   if (consume(TK_SIZEOF)) {
-    Node *node = calloc(1, sizeof(Node));
-    node->op = ND_SIZEOF;
-    node->expr = unary();
-    return node;
+    return new_expr(ND_SIZEOF, unary());
   }
   return postfix();
 }
@@ -118,7 +114,7 @@ static Node *mul() {
     if (t->ty != '*' && t->ty != '/')
       return lhs;
     pos++;
-    lhs = new_node(t->ty, lhs, unary());
+    lhs = new_binop(t->ty, lhs, unary());
   }
 }
 
@@ -129,7 +125,7 @@ static Node *add() {
     if (t->ty != '+' && t->ty != '-')
       return lhs;
     pos++;
-    lhs = new_node(t->ty, lhs, mul());
+    lhs = new_binop(t->ty, lhs, mul());
   }
 }
 
@@ -139,12 +135,12 @@ static Node *rel() {
     Token *t = tokens->data[pos];
     if (t->ty == '<') {
       pos++;
-      lhs = new_node('<', lhs, add());
+      lhs = new_binop('<', lhs, add());
       continue;
     }
     if (t->ty == '>') {
       pos++;
-      lhs = new_node('<', add(), lhs);
+      lhs = new_binop('<', add(), lhs);
       continue;
     }
     return lhs;
@@ -158,7 +154,7 @@ static Node *logand() {
     if (t->ty != TK_LOGAND)
       return lhs;
     pos++;
-    lhs = new_node(ND_LOGAND, lhs, rel());
+    lhs = new_binop(ND_LOGAND, lhs, rel());
   }
 }
 
@@ -169,14 +165,14 @@ static Node *logor() {
     if (t->ty != TK_LOGOR)
       return lhs;
     pos++;
-    lhs = new_node(ND_LOGOR, lhs, logand());
+    lhs = new_binop(ND_LOGOR, lhs, logand());
   }
 }
 
 static Node *assign() {
   Node *lhs = logor();
   if (consume('='))
-    return new_node('=', lhs, logor());
+    return new_binop('=', lhs, logor());
   return lhs;
 }
 
@@ -246,9 +242,7 @@ static Node *param() {
 }
 
 static Node *expr_stmt() {
-  Node *node = calloc(1, sizeof(Node));
-  node->op = ND_EXPR_STMT;
-  node->expr = assign();
+  Node *node = new_expr(ND_EXPR_STMT, assign());
   expect(';');
   return node;
 }
