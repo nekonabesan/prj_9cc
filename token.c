156410363511c84de55f3204152b07ffdb1a1fb7
nekonabesan <sheepdogs_2@hotmail.com>
2019-09-24 10:29:39 +0900

    Add "extern".
    https://github.com/rui314/9cc/commit/59ddb6ee91e50a879bb93b136e0602094ead4a9f

--- token.c
+++ token.c
@@ -14,7 +14,7 @@ static struct {
   int ty;
 } symbols[] = {
   {"char", TK_CHAR},     {"do", TK_DO},
-  {"else", TK_ELSE},     {"for", TK_FOR},
+  {"else", TK_ELSE},     {"extern", TK_EXTERN},{"for", TK_FOR},
   {"if", TK_IF},         {"int", TK_INT},
   {"return", TK_RETURN}, {"sizeof", TK_SIZEOF},
   {"while", TK_WHILE},   {"&&", TK_LOGAND},
